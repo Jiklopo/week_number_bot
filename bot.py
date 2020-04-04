3671362b88e6a94c5e1ec69cbf765d60e4bf7210
jiklopo <itima.com@gmail.com>
2020-04-04 20:28:37 +0600

    You have to use Flask or something to open port or Heroku will kill the bot

--- bot.py
+++ bot.py
@@ -1,22 +1,29 @@
 import os
-import time
+import telebot
+from flask import Flask, request
 from telebot import TeleBot
 from week import get_current_week as week
 
 TOKEN = os.getenv('TOKEN')
 bot = TeleBot(TOKEN)
-
+server = Flask(__name__)
 
 @bot.message_handler(commands="week")
 def week_number(message):
     bot.reply_to(message, f"Текущая неделя: {week()}")
 
 
-while True:
-    try:
-        print('Starting...')
-        bot.polling()
-    except:
-        print('Some error has occurred.')
-    finally:
-        print('Restarting...')
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://test-new-new.herokuapp.com/' + TOKEN)
+    return "!", 200
+
+
+server.debug = True
+server.run(host="0.0.0.0", port=5000)
