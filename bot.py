e69d9ca125a58ce5ebe7dd440def9010c7dd0f81
jiklopo <itima.com@gmail.com>
2020-04-04 20:38:52 +0600

    You have to use Flask or something to open port or Heroku will kill the bot

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os
 import telebot
+import logging
 from flask import Flask, request
 from telebot import TeleBot
 from week import get_current_week as week
@@ -8,22 +9,33 @@ TOKEN = os.getenv('TOKEN')
 bot = TeleBot(TOKEN)
 server = Flask(__name__)
 
+
 @bot.message_handler(commands="week")
 def week_number(message):
     bot.reply_to(message, f"Текущая неделя: {week()}")
 
 
-@server.route('/' + TOKEN, methods=['POST'])
-def getMessage():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "!", 200
+if "HEROKU" in list(os.environ.keys()):
+    logger = telebot.logger
+    telebot.logger.setLevel(logging.INFO)
 
-@server.route("/")
-def webhook():
+    server = Flask(__name__)
+
+
+    @server.route("/bot", methods=['POST'])
+    def getMessage():
+        bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+        return "!", 200
+
+
+    @server.route("/")
+    def webhook():
+        bot.remove_webhook()
+        bot.set_webhook(url="https://week-number-bot.herokuapp.com/")
+        return "?", 200
+
+
+    server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
+else:
     bot.remove_webhook()
-    bot.set_webhook(url='https://test-new-new.herokuapp.com/' + TOKEN)
-    return "!", 200
-
-
-server.debug = True
-server.run(host="0.0.0.0", port=5000)
+    bot.polling(none_stop=True)
