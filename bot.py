33d792360b4bcf48b8c7db4638c9749a26e38e3d
jiklopo <itima.com@gmail.com>
2020-04-04 20:44:54 +0600

    Testing webhooks

--- bot.py
+++ bot.py
@@ -15,27 +15,20 @@ def week_number(message):
     bot.reply_to(message, f"Текущая неделя: {week()}")
 
 
-if "HEROKU" in list(os.environ.keys()):
-    logger = telebot.logger
-    telebot.logger.setLevel(logging.INFO)
-
-    server = Flask(__name__)
-
-
-    @server.route("/bot", methods=['POST'])
-    def getMessage():
-        bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-        return "!", 200
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
 
 
-    @server.route("/")
-    def webhook():
-        bot.remove_webhook()
-        bot.set_webhook(url="https://week-number-bot.herokuapp.com/")
-        return "?", 200
-
-
-    server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
-else:
+@server.route("/")
+def webhook():
     bot.remove_webhook()
-    bot.polling(none_stop=True)
+    bot.set_webhook(url='https://week-number-bot.herokuapp.com/' + TOKEN)
+    return "!", 200
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+
+
